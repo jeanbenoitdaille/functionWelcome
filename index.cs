89593a7bb441495fd014829e141146ee12e1217b
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-15 14:17:39 +0100

    Add files via upload

---
+++ index.cs
@@ -0,0 +1,11 @@
+     using System;
+     
+    namespace Coding.Exercise
+    {
+        public class Exercise
+        {
+            public string SayHello(string name){
+                return $"Bonjour {name} !";
+            }
+        }
+    }
